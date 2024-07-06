e3e3134c69a0c20cb52cb15dc4e07d2950d81575
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-07-06 18:13:15 +0800

    Added study of energy vs range

--- examples/energy_vs_range.py
+++ examples/energy_vs_range.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
 	evtol1_params['battery_max_discharge'] 	= 0.8
 	# Design parameters
 	evtol1_weight 			= 1500.0 # kg
-	evtol1_r_rotor_lift		= 1.2 # m
-	evtol1_cruise_speed 	= 50.0 # m/s
+	evtol1_r_rotor_lift		= 1.0 # m
+	evtol1_cruise_speed 	= 30.0 # m/s
 	evtol1_rotor_mu 		= 0.3
 
 	# Constants
@@ -45,10 +45,10 @@ if __name__ == '__main__':
 	evtol2_params['battery_max_discharge'] 	= 0.8
 	# Design parameters
 	evtol2_weight 			= 1500.0 # kg
-	evtol2_r_rotor_lift		= 1.2 # m
-	evtol2_r_rotor_cruise 	= 0.9 # m
-	evtol2_cruise_speed 	= 50.0 # m/s
-	evtol2_wing_area 		= 6.4 # m**2
+	evtol2_r_rotor_lift		= 1.0 # m
+	evtol2_r_rotor_cruise 	= 1.0 # m
+	evtol2_cruise_speed 	= 30.0 # m/s
+	evtol2_wing_area 		= 8.0 # m**2
 	evtol2_rotor_J 			= 1.0
 
 	# Constants
@@ -57,18 +57,22 @@ if __name__ == '__main__':
 	evtol2_params['gravitational_accel'] 	= 9.81 # kg/m**3
 
 	# --- Mission requirements --- #
-	n_missions		= 20
-	flight_ranges 	= np.linspace(50.0, 20000.0, n_missions) # m
-	hover_times 	= n_missions * [240.0] # s
+	n_missions1		= 30
+	flight_ranges1 	= np.linspace(50.0, 30000.0, n_missions1) # m
+	hover_times1 	= n_missions1 * [240.0] # s
+	n_missions2		= 78
+	flight_ranges2 	= np.linspace(50.0, 78000.0, n_missions2) # m
+	hover_times2 	= n_missions2 * [240.0] # s
 
 	# --- Energy consumption for Wingless Multirotor --- #
-	energy_list1 = np.zeros(n_missions)
+	energy_list1 = np.zeros(n_missions1)
 
-	for i in range(n_missions):
+	for i in range(n_missions1):
+		print(f'Solving Wingless Multirotor; range = {flight_ranges1[i]/1000} km')
 		prob = om.Problem()
 		indeps = prob.model.add_subsystem('indeps', om.IndepVarComp(), promotes=['*'])
-		indeps.add_output('flight_distance', flight_ranges[i], units='m')
-		indeps.add_output('hover_time', hover_times[i], units='s')
+		indeps.add_output('flight_distance', flight_ranges1[i], units='m')
+		indeps.add_output('hover_time', hover_times1[i], units='s')
 		indeps.add_output('eVTOL|W_takeoff', evtol1_weight, units='kg')
 		indeps.add_output('eVTOL|Cruise_speed', evtol1_cruise_speed, units='m/s')
 		indeps.add_output('Rotor|radius_lift', evtol1_r_rotor_lift, units='m')
@@ -84,13 +88,14 @@ if __name__ == '__main__':
 		energy_list1[i] = prob.get_val('energy_cnsmp', 'kW*h')[0]
 
 	# --- Energy consumption for Lift+Cruise --- #
-	energy_list2 = np.zeros(n_missions)
+	energy_list2 = np.zeros(n_missions2)
 
-	for i in range(n_missions):
+	for i in range(n_missions2):
+		print(f'Solving Winged Lift+Cruise; range = {flight_ranges2[i]/1000} km')
 		prob = om.Problem()
 		indeps = prob.model.add_subsystem('indeps', om.IndepVarComp(), promotes=['*'])
-		indeps.add_output('flight_distance', flight_ranges[i], units='m')
-		indeps.add_output('hover_time', hover_times[i], units='s')
+		indeps.add_output('flight_distance', flight_ranges2[i], units='m')
+		indeps.add_output('hover_time', hover_times2[i], units='s')
 		indeps.add_output('eVTOL|W_takeoff', evtol2_weight, units='kg')
 		indeps.add_output('eVTOL|Cruise_speed', evtol2_cruise_speed, units='m/s')
 		indeps.add_output('Rotor|radius_lift', evtol2_r_rotor_lift, units='m')
@@ -108,12 +113,13 @@ if __name__ == '__main__':
 		energy_list2[i] = prob.get_val('energy_cnsmp', 'kW*h')[0]
 
 	
-	plt.plot(flight_ranges/1000.0, energy_list1, label='Wingless Multirotor')
-	plt.plot(flight_ranges/1000.0, energy_list2, label='Winged Lift+Cruise')
+	plt.plot(flight_ranges1/1000.0, energy_list1, label='Wingless Multirotor')
+	plt.plot(flight_ranges2/1000.0, energy_list2, label='Winged Lift+Cruise')
 	plt.xlabel('Mission range [km]')
 	plt.ylabel('Energy consumption [kWh]')
 	plt.title('Energy consumption vs mission range')
-	plt.legend()
+	plt.legend(loc='lower right')
+	plt.grid(True)
 	plt.show()
 
 
