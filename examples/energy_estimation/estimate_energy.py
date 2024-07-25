d409ad7feb599ca874065f5a4401b7e48be6a30d
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-07-25 18:58:32 +0800

    Added energy estimation example

---
+++ examples/energy_estimation/estimate_energy.py
@@ -0,0 +1,14 @@
+from MCEVS.Missions.Standard import ConstantHoverAndCruiseMissionProfile
+from MCEVS.Utils.Plots import plot_mission_parameters
+
+# Mission range
+x = 96600 # m
+
+# Cruise speed
+v = 67.1 # m/s
+
+mission = ConstantHoverAndCruiseMissionProfile(mission_range=x, cruise_speed=v)
+# plot_mission_parameters(mission, print_info=True)
+
+
+print(mission.segments[3])
