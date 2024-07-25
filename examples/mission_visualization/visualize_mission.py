974c3c9eae945c469d9676a9f6196bf950e5ec22
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-07-25 18:59:12 +0800

    Working example of mission parameters plotting

--- examples/mission_visualization/visualize_mission.py
+++ examples/mission_visualization/visualize_mission.py
@@ -1,12 +1,38 @@
 import numpy as np
-from MCEVS.Missions.Standard import UberMissionRequirement
-from MCEVS.Missions.Standard import SimpleMissionRequirement
+from MCEVS.Missions.Container import Mission
+from MCEVS.Missions.Standard import UberMissionProfile
+from MCEVS.Missions.Standard import SimplifiedUberMissionProfile
+from MCEVS.Missions.Standard import StandardMissionProfile
+from MCEVS.Missions.Standard import ConstantHoverAndCruiseMissionProfile
+from MCEVS.Utils.Plots import plot_mission_parameters
 
+# Mission range
+x = 96600 # m
 
-# mission = UberMissionRequirement(v_stall=40)
-# # mission.print()
-# mission.visualize()
-# 96600
-mission = SimpleMissionRequirement(mission_range=96600, cruise_speed=67.1)
-# mission.print()
-mission.visualize()
+# Cruise speed
+v = 67.1 # m/s
+
+plot_uber = False
+plot_simplified_uber = False
+plot_standard = False
+plot_constantHAC = False
+
+# --- Uber Mission Requirement --- #
+if plot_uber:
+	mission = UberMissionProfile(v_stall=40)
+	plot_mission_parameters(mission, print_info=True)
+
+# --- Simplified Uber Mission Requirement --- #
+if plot_simplified_uber:
+	mission = SimplifiedUberMissionProfile(mission_range=x, cruise_speed=v)
+	plot_mission_parameters(mission, print_info=True)
+
+# --- Standard Mission Requirement --- #
+if plot_standard:
+	mission = StandardMissionProfile(mission_range=x, cruise_speed=v)
+	plot_mission_parameters(mission, print_info=True)
+
+# --- ConstantHoverAndCruise Mission Requirement --- #
+if plot_constantHAC:
+	mission = ConstantHoverAndCruiseMissionProfile(mission_range=x, cruise_speed=v)
+	plot_mission_parameters(mission, print_info=True)
