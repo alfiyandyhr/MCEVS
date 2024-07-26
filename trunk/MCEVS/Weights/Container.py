56050b82eea228c625da6340eadcc9c0972e86c0
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-07-26 19:59:39 +0800

    Added a container for VehicleWeight

---
+++ trunk/MCEVS/Weights/Container.py
@@ -0,0 +1,42 @@
+class VehicleWeight(object):
+	"""
+	docstring for VehicleWeight
+	Weight of the vehicle.
+
+	Top level info:
+		> Max take-off weight
+		> Payload weight
+		> Battery weight
+		> Propulsion weight
+		> Structure weight
+		> Equipment weight
+
+	Broken down into:
+		> Payload
+		> Battery
+		> Propulsion:
+			- Motor
+			- Controller
+			- Rotors
+		> Structure:
+			- Fuselage
+			- Landing_Gear
+			- Empennage
+			- Wing (if any)
+		> Equipment:
+			- Anti-icing
+			- Avionics
+			- Flight_Control
+			- Furnishings 
+	"""
+	def __init__(self, mtow=None):
+		super(VehicleWeight, self).__init__()
+		self.max_takeoff = mtow	# defined, or sized
+		self.payload = None
+		self.battery = None
+		self.propulsion = None
+		self.structure = None
+		self.equipment = None
+
+	def is_being_sized(self):
+		pass
