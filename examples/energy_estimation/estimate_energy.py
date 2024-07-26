ca229bcb223ac4d151996373689eb94c981d3d99
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-07-26 20:01:16 +0800

    Working on examples of creating a vehicle and its analysis

--- examples/energy_estimation/estimate_energy.py
+++ examples/energy_estimation/estimate_energy.py
@@ -1,11 +1,27 @@
+from MCEVS.Vehicles.Standard import StandardMultirotorEVTOL
+from MCEVS.Vehicles.Standard import StandardLiftPlusCruiseEVTOL
 from MCEVS.Missions.Standard import ConstantHoverAndCruiseMissionProfile
+from MCEVS.Analyses.Power import PowerAnalysis
 from MCEVS.Utils.Plots import plot_mission_parameters
 
-# Mission range
-x = 96600 # m
+# Mission requirement
+mission_range = 30000 # m
+cruise_speed = 30.0 # m/s
 
-# Cruise speed
-v = 67.1 # m/s
+mission = ConstantHoverAndCruiseMissionProfile(mission_range=mission_range, cruise_speed=cruise_speed)
+# plot_mission_parameters(mission, print_info=False)
 
-mission = ConstantHoverAndCruiseMissionProfile(mission_range=x, cruise_speed=v)
-# plot_mission_parameters(mission, print_info=True)
+# Design variables (aircraft design and operation)
+# design_var1 = {'r_lift_rotor': 1.0, 'cruise_speed': cruise_speed, 'lift_rotor_mu': 0.3}
+design_var2 = {'wing_area': 8.0, 'wing_AR': 10.0,
+			  'r_lift_rotor': 1.0, 'r_propeller': 1.0,
+			  'cruise_speed': cruise_speed, 'propeller_J': 1.0}
+
+# vehicle1 = StandardMultirotorEVTOL(design_var1)
+# vehicle1.weight.max_takeoff = 1500.0
+
+vehicle2 = StandardLiftPlusCruiseEVTOL(design_var2)
+vehicle2.weight.max_takeoff = 1500.0
+
+analysis = PowerAnalysis(vehicle2, mission)
+results = analysis.evaluate()
--- examples/vehicle_creation/create_vehicle.py
+++ examples/vehicle_creation/create_vehicle.py
@@ -1,8 +1,13 @@
+from MCEVS.Vehicles.Standard import StandardMultirotorEVTOL
 from MCEVS.Vehicles.Standard import StandardLiftPlusCruiseEVTOL
 
-design_var = {'wing_area': 8.0, 'wing_AR': 10.0,
-			  'r_rotor_lift': 1.0, 'r_rotor_cruise': 1.0,
-			  'cruise_speed': 40.0, 'propeller_J': 1.0}
+design_var1 = {'r_lift_rotor': 1.0}
 
-vehicle = StandardLiftPlusCruiseEVTOL(design_var)
-# vehicle.print_info()
+design_var2 = {'wing_area': 8.0, 'wing_AR': 10.0,
+			   'r_rotor_lift': 1.0, 'r_rotor_cruise': 1.0}
+
+vehicle1 = StandardMultirotorEVTOL(design_var1)
+vehicle1.print_info()
+
+vehicle2 = StandardLiftPlusCruiseEVTOL(design_var2)
+vehicle2.print_info()
