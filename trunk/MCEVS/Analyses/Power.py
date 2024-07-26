a233ef47e5d5913eccb799445a22490e3c0a7b42
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-07-26 19:58:50 +0800

    Started writing power analysis

---
+++ trunk/MCEVS/Analyses/Power.py
@@ -0,0 +1,12 @@
+class PowerAnalysis(object):
+	"""
+	docstring for PowerAnalysis
+	"""
+	def __init__(self, vehicle:object, mission:object):
+		super(PowerAnalysis, self).__init__()
+		self.vehicle = vehicle
+		self.mission = mission
+
+	def evaluate(self):
+		return None
+		
