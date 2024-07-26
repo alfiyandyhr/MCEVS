8e64496547edb8a6c6f630b17d14b10e7fe80206
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-07-26 19:58:16 +0800

    Modified the vehicle container and standard

--- trunk/MCEVS/Vehicles/Container.py
+++ trunk/MCEVS/Vehicles/Container.py
@@ -4,6 +4,8 @@ from MCEVS.Vehicles.Components.Landing_Gear import LandingGear
 from MCEVS.Vehicles.Components.Rotors import LiftRotor, Propeller
 from MCEVS.Vehicles.Components.Battery import Battery
 
+from MCEVS.Weights.Container import VehicleWeight
+
 import numpy as np
 
 class LiftPlusCruiseEVTOL(object):
@@ -11,7 +13,7 @@ class LiftPlusCruiseEVTOL(object):
 	docstring for LiftPlusCruiseEVTOL
 	This is a type of eVTOL that has a fixed-wing and separate propulsion for hover and cruise.
 	"""
-	def __init__(self):
+	def __init__(self, mtow=None):
 		super(LiftPlusCruiseEVTOL, self).__init__()
 		
 		self.configuration = 'LiftPlusCruise'
@@ -23,12 +25,15 @@ class LiftPlusCruiseEVTOL(object):
 		self.landing_gear = None
 		self.empennage = None
 		self.lift_rotor = None
-		self.cruise_propeller = None
+		self.propeller = None
 		self.battery = None
 
 		# Weight and performance
-		self.mass_properties = None
+		self.weight = VehicleWeight(mtow)
 		self.performance = None
+
+		# Whether or not this vehicle has been sized
+		self.is_sized = False
 
 	def add_component(self, kind:str, **kwargs):
 
@@ -65,6 +70,57 @@ class LiftPlusCruiseEVTOL(object):
 		print(self.landing_gear._info())
 		print(self.lift_rotor._info())
 		print(self.propeller._info())
+
+class MultirotorEVTOL(object):
+	"""
+	docstring for MultirotorEVTOL
+	This is a type of eVTOL that has a fixed-wing and separate propulsion for hover and cruise.
+	"""
+	def __init__(self, mtow=None):
+		super(MultirotorEVTOL, self).__init__()
+		
+		self.configuration = 'Multirotor'
+		self.n_passenger = None # including the pilot
+
+		# Components
+		self.fuselage = None
+		self.landing_gear = None
+		self.empennage = None
+		self.lift_rotor = None
+		self.battery = None
+
+		# Weight and performance
+		self.weight = VehicleWeight(mtow)
+		self.performance = None
+
+		# Whether or not this vehicle has been sized
+		self.is_sized = False
+
+	def add_component(self, kind:str, **kwargs):
+
+		if kind == 'battery':
+			self.battery = Battery(kwargs)
+			self.battery._initialize()
+
+		elif kind == 'fuselage':
+			self.fuselage = Fuselage(kwargs)
+			self.fuselage._initialize()
+
+		elif kind == 'landing_gear':
+			self.landing_gear = LandingGear(kwargs)
+			self.landing_gear._initialize()
+
+		elif kind == 'lift_rotor':
+			self.lift_rotor = LiftRotor(kwargs)
+			self.lift_rotor._initialize()
+
+	def print_info(self):
+		print('Vehicle type: Multirotor eVTOL')
+		print('List of components:')
+		print(self.battery._info())
+		print(self.fuselage._info())
+		print(self.landing_gear._info())
+		print(self.lift_rotor._info())
 
 
 
--- trunk/MCEVS/Vehicles/Standard.py
+++ trunk/MCEVS/Vehicles/Standard.py
@@ -1,18 +1,31 @@
+from MCEVS.Vehicles.Container import MultirotorEVTOL
 from MCEVS.Vehicles.Container import LiftPlusCruiseEVTOL
+
+def StandardMultirotorEVTOL(design_var:dict):
+
+	r_lift_rotor = design_var['r_lift_rotor']
+
+	vehicle = MultirotorEVTOL()
+	vehicle.add_component(kind='battery', density=250, efficiency=0.85, max_discharge=0.8)
+	vehicle.add_component(kind='fuselage', length=1.8, max_perimeter=6.7)
+	vehicle.add_component(kind='landing_gear', strut_length=0.7, ultimate_load_factor=5.7)
+	vehicle.add_component(kind='lift_rotor', n_rotor=7, solidity=0.13, radius=r_lift_rotor)
+	
+	return vehicle
 
 def StandardLiftPlusCruiseEVTOL(design_var:dict):
 
 	wing_area = design_var['wing_area']
 	wing_AR = design_var['wing_AR']
-	r_rotor_lift = design_var['r_rotor_lift']
-	r_rotor_cruise = design_var['r_rotor_cruise']
+	r_lift_rotor = design_var['r_lift_rotor']
+	r_propeller = design_var['r_propeller']
 
 	vehicle = LiftPlusCruiseEVTOL()
 	vehicle.add_component(kind='battery', density=250, efficiency=0.85, max_discharge=0.8)
 	vehicle.add_component(kind='wing', area=wing_area, aspect_ratio=wing_AR, ultimate_load_factor=3.0)
 	vehicle.add_component(kind='fuselage', length=1.8, max_perimeter=6.7)
 	vehicle.add_component(kind='landing_gear', strut_length=0.7, ultimate_load_factor=5.7)
-	vehicle.add_component(kind='lift_rotor', n_rotor=6, solidity=0.13, radius=r_rotor_lift)
-	vehicle.add_component(kind='propeller', n_propeller=1, solidity=0.13, radius=r_rotor_cruise)
+	vehicle.add_component(kind='lift_rotor', n_rotor=6, solidity=0.13, radius=r_lift_rotor)
+	vehicle.add_component(kind='propeller', n_propeller=1, solidity=0.13, radius=r_propeller)
 	
 	return vehicle
