a57ebcb3aa115023cc62d05982821ddca073b1bb
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-08-04 00:05:37 +0800

    Initialized optimization modules

---
+++ trunk/MCEVS/Optimization/Container.py
@@ -0,0 +1,169 @@
+import numpy as np
+import openmdao.api as om
+from MCEVS.Analyses.Weight.Analysis import WeightAnalysis
+from MCEVS.Vehicles.Standard import StandardLiftPlusCruiseEVTOL, StandardMultirotorEVTOL
+from MCEVS.Optimization.Gradient_Based.Algorithm import run_gradient_based_optimization
+from MCEVS.Optimization.Gradient_Free.Algorithm import run_gradient_free_optimization
+
+class DesignProblem(object):
+	"""
+	docstring for DesignProblem
+	This class object contains the problem definition of the eVTOL design
+
+	minimize 		: objective functions
+	with respect to : design variables
+	subject to 		: inequality constraints, equality constraints
+	under			: design conditions
+
+	"""
+	def __init__(self, vehicle=object, mission=object, constants=object, algorithm='str'):
+		super(DesignProblem, self).__init__()
+		self.kind = None # ['SingleObjectiveProblem', 'MultiObjectiveProblem']
+
+		# MCEVS core objects
+		self.vehicle 	= vehicle
+		self.mission 	= mission
+		self.constants 	= constants
+
+		# Design problem
+		self.objectives				= None
+		self.design_variables		= {}
+		self.constraints 			= {}
+
+		# The driver
+		self.algorithm		= algorithm
+
+		# Initial, final, and optimal designs
+		self.initial_design			= None
+		self.final_design 			= None
+		self.optimal_design			= None
+
+	def run_optimization(self):
+		"""
+		Run the optimization and actually solve the problem
+		"""
+		if self.algorithm == 'gradient-based':
+			# Run !!!
+			run_gradient_based_optimization(self)
+
+		elif self.algorithm == 'gradient-free':
+			# Run !!!
+			run_gradient_free_optimization(self)
+
+	def add_objective(self, name):
+		"""
+		Inputs:
+			name: name of the variables in OpenMDAO format
+				available:
+					- Mission|cruise_speed
+					- Wing|area
+						  |aspect_ratio
+					- LiftRotor|radius
+							   |advance_ratio
+					- Propeller|radius
+							   |advance_ratio
+		"""
+		self.objectives = name
+
+	def add_design_var(self, name:str, lower:float, upper:float, units=None):
+		"""
+		Inputs:
+			name: name of the variables in OpenMDAO format
+				available:
+					- Mission|cruise_speed
+					- Wing|area
+						  |aspect_ratio
+					- LiftRotor|radius
+							   |advance_ratio
+					- Propeller|radius
+							   |advance_ratio
+			lower: lower bound
+			upper: upper bound
+			units: units
+		"""
+		if name == 'Mission|cruise_speed':
+			for segment in self.mission.segments:
+				if segment.kind == 'CruiseConstantSpeed':
+					init = segment.speed
+					break
+
+		elif name == 'Wing|area':
+			init = self.vehicle.wing.area
+
+		elif name == 'Wing|aspect_ratio':
+			init = self.vehicle.wing.aspect_ratio
+
+		elif name == 'LiftRotor|radius':
+			init = self.vehicle.lift_rotor.radius
+
+		elif name == 'LiftRotor|advance_ratio':
+			init = self.vehicle.lift_rotor.advance_ratio
+
+		elif name == 'Propeller|radius':
+			init = self.vehicle.propeller.radius
+
+		elif name == 'Propeller|advance_ratio':
+			init = self.vehicle.propeller.advance_ratio
+
+		# Append design variable info
+		self.design_variables[name] = [lower, upper, init, units]
+
+	def add_constraint(self, name, lower=float, upper=float, units=None):
+		"""
+		Inputs:
+			name: name of the variables in OpenMDAO format
+				available:
+					- Mission|cruise_speed
+					- Wing|area
+						  |aspect_ratio
+					- LiftRotor|radius
+							   |advance_ratio
+					- Propeller|radius
+							   |advance_ratio
+			lower: lower bound
+			upper: upper bound
+			units: units
+		"""
+		self.constraints[name] = [lower, upper, units]		
+
+	def evaluate_one_sample(self, x:dict):
+		"""
+		Given the design variable dictionary, evaluate the objective and constraints
+		"""
+		
+		if self.vehicle.configuration == 'Multirotor':
+			vehicle = StandardMultirotorEVTOL(x)
+
+		elif self.vehicle.configuration == 'LiftPlusCruise':
+			vehicle = StandardLiftPlusCruiseEVTOL(x)
+
+		# Analysis
+		analysis = WeightAnalysis(vehicle=vehicle,
+								  mission=self.mission,
+								  constants=self.constants,
+								  sizing_mode=True)
+		
+		results = analysis.evaluate()
+
+		f = results.get_val('Weight|takeoff', 'kg')
+
+		return f
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ trunk/MCEVS/Optimization/Gradient_Based/Algorithm.py
@@ -0,0 +1,63 @@
+import openmdao.api as om
+
+from MCEVS.Analyses.Weight.Analysis import MTOWEstimation
+
+def run_gradient_based_optimization(DesignProblem:object):
+
+	prob = om.Problem()
+
+	indeps = prob.model.add_subsystem('indeps', om.IndepVarComp(), promotes=['*'])
+
+	# Initial values
+	for var_name in DesignProblem.design_variables:
+		indeps.add_output(var_name,
+						  DesignProblem.design_variables[var_name][2],
+						  units=DesignProblem.design_variables[var_name][3])
+	
+	# Objective function
+	prob.model.add_objective(DesignProblem.objectives)
+
+	# Design variables and their boundaries
+	for var_name in DesignProblem.design_variables:
+		prob.model.add_design_var(var_name,
+								  lower=DesignProblem.design_variables[var_name][0],
+								  upper=DesignProblem.design_variables[var_name][1],
+								  units=DesignProblem.design_variables[var_name][3])
+
+	# Constraints
+	for cnstr_name in DesignProblem.constraints:
+		prob.model.add_constraint(cnstr_name,
+								  lower=DesignProblem.constraints[cnstr_name][0],
+								  upper=DesignProblem.constraints[cnstr_name][1],
+								  units=DesignProblem.constraints[cnstr_name][2])
+
+	# Analysis
+	if DesignProblem.objectives == 'Weight|takeoff':
+		prob.model.add_subsystem('weight_estimation',
+								  MTOWEstimation(mission=DesignProblem.mission,
+								  				 vehicle=DesignProblem.vehicle,
+								  				 constants=DesignProblem.constants,
+								  				 sizing_mode=True),
+								  promotes_inputs=['*'],
+								  promotes_outputs=['*'])
+
+	# Optimizer settings
+	prob.driver = om.ScipyOptimizeDriver(optimizer='SLSQP', tol=1e-8, disp=True)
+	prob.driver.recording_options['includes'] = ['*']
+	prob.driver.recording_options['record_objectives'] = True
+	prob.driver.recording_options['record_constraints'] = True
+	prob.driver.recording_options['record_desvars'] = True
+	prob.driver.recording_options['record_inputs'] = True
+	prob.driver.recording_options['record_outputs'] = True
+	prob.driver.recording_options['record_residuals'] = True
+
+	if DesignProblem.vehicle.configuration == 'Multirotor':
+		prob.driver.add_recorder(om.SqliteRecorder("multirotor_opt_cases.sql"))
+	elif DesignProblem.vehicle.configuration == 'LiftPlusCruise':
+		prob.driver.add_recorder(om.SqliteRecorder("liftcruise_opt_cases.sql"))
+
+	# Run optimization
+	prob.setup(check=False)
+	prob.run_driver()
+
+	return prob
