494460956faea53771a3e5030fad813afe0674e9
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-08-04 00:18:38 +0800

    Added new dependency: pymoo

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
 	name = "MCEVS",
 	version = "0.0.1",
-	description = "MCEVS: Alfi eVTOL; Multi-configurational eVTOL Sizing Codes",
+	description = "MCEVS: Multi-configurational eVTOL Sizing Codes",
 	license="",
 	packages=["MCEVS"],
 	package_dir={"" : "trunk"},
-	install_requires=["numpy", "openmdao>=3.16.0", "matplotlib"]
+	install_requires=["numpy", "openmdao>=3.16.0", "matplotlib", "pymoo>=0.6.0.1"]
 	)
