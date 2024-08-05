92c47eb7ee3122bafb40cb4dc16e803c1d873a31
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-08-05 19:41:05 +0800

    New parameters for mtow estimation

--- examples/mtow_estimation/estimate_mtow_multirotor.py
+++ examples/mtow_estimation/estimate_mtow_multirotor.py
@@ -12,9 +12,9 @@ payload_weight = 400.0
 # Take-off from 5000 ft ASL or 1500 m
 mission = Mission(takeoff_altitude=0.0)
 # mission.add_segment('Hover Climb', kind='HoverStay', duration=120.0, n_discrete=5)
-mission.add_segment(name='Hover Climb', kind='HoverClimbConstantSpeed', speed=2.54, distance=152.4, n_discrete=10)
+mission.add_segment(name='Hover Climb', kind='HoverClimbConstantSpeed', speed=2.54, distance=304.8, n_discrete=10)
 mission.add_segment('Cruise', kind='CruiseConstantSpeed', AoA=5, speed=cruise_speed, distance=mission_range, n_discrete=5)
-mission.add_segment(name='Hover Descent', kind='HoverDescentConstantSpeed', speed=1.524, distance=152.4, n_discrete=10)
+mission.add_segment(name='Hover Descent', kind='HoverDescentConstantSpeed', speed=1.524, distance=304.8, n_discrete=10)
 # mission.add_segment('Hover Descent', kind='HoverStay', duration=120.0, n_discrete=5)
 # plot_mission_parameters(mission, print_info=False)
 
