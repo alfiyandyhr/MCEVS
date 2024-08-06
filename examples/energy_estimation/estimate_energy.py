22ae62e79b5d8d154deb073e05461ac2dc9345fd
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-08-07 03:46:45 +0800

    Working examples for hover, climb, cruise, descent mission

--- examples/energy_estimation/estimate_energy.py
+++ examples/energy_estimation/estimate_energy.py
@@ -1,3 +1,4 @@
+import numpy as np
 from MCEVS.Vehicles.Standard import StandardMultirotorEVTOL
 from MCEVS.Vehicles.Standard import StandardLiftPlusCruiseEVTOL
 from MCEVS.Missions.Container import Mission
@@ -10,11 +11,18 @@ mission_range = 30000 # m
 cruise_speed = 30.0 # m/s
 payload_weight = 400.0
 
+# Cruise range
+climb_dist_X   = np.sqrt(cruise_speed**2 - 2.54**2) * (304.8/2.54)
+descent_dist_X = np.sqrt(cruise_speed**2 - 1.524**2) * (304.8/1.524)
+cruise_range   = mission_range - climb_dist_X - descent_dist_X
+
 # Take-off from 5000 ft ASL or 1500 m
 mission = Mission(takeoff_altitude=0.0)
 # mission.add_segment('Hover Climb', kind='HoverStay', duration=120.0, n_discrete=5)
 mission.add_segment(name='Hover Climb', kind='HoverClimbConstantSpeed', speed=2.54, distance=152.4, n_discrete=10)
-mission.add_segment('Cruise', kind='CruiseConstantSpeed', AoA=5, speed=cruise_speed, distance=mission_range, n_discrete=5)
+mission.add_segment(name='Constant Climb', kind='ClimbConstantVyConstantVx', distance_Y=304.8, speed_Y=2.54, speed=cruise_speed, n_discrete=10)
+mission.add_segment('Cruise', kind='CruiseConstantSpeed', AoA=5, speed=cruise_speed, distance=cruise_range, n_discrete=10)
+mission.add_segment(name='Constant Descent', kind='DescentConstantVyConstantVx', distance_Y=304.8, speed_Y=1.524, speed=cruise_speed, n_discrete=10)
 mission.add_segment(name='Hover Descent', kind='HoverDescentConstantSpeed', speed=1.524, distance=152.4, n_discrete=10)
 # mission.add_segment('Hover Descent', kind='HoverStay', duration=120.0, n_discrete=5)
 # plot_mission_parameters(mission, print_info=False)
@@ -29,15 +37,18 @@ design_var2 = {'wing_area': 8.0, 'wing_aspect_ratio': 10.0,
 			  'cruise_speed': cruise_speed, 'propeller_advance_ratio': 1.0}
 
 vehicle1 = StandardMultirotorEVTOL(design_var1, mtow=878.439221)
-vehicle2 = StandardLiftPlusCruiseEVTOL(design_var2, mtow=945.145355)
+vehicle2 = StandardLiftPlusCruiseEVTOL(design_var2, mtow=983.1065278)
 
 # Analysis
 analysis = EnergyAnalysis(vehicle=vehicle2, mission=mission, constants=constants)
 results = analysis.evaluate()
 
+
 print(results.get_val('Power|segment_1', 'W'))
 print(results.get_val('Power|segment_2', 'W'))
 print(results.get_val('Power|segment_3', 'W'))
+print(results.get_val('Power|segment_4', 'W'))
+print(results.get_val('Power|segment_5', 'W'))
 print(results.get_val('Energy|entire_mission', 'kW*h'))
 
 
--- examples/mtow_estimation/estimate_mtow_liftcruise.py
+++ examples/mtow_estimation/estimate_mtow_liftcruise.py
@@ -1,3 +1,4 @@
+import numpy as np
 from MCEVS.Vehicles.Standard import StandardLiftPlusCruiseEVTOL
 from MCEVS.Missions.Container import Mission
 from MCEVS.Constants.Container import EarthGravityAndAtmosphere
@@ -9,12 +10,19 @@ mission_range = 30000 # m
 cruise_speed = 30.0 # m/s
 payload_weight = 400.0
 
+# Cruise range
+climb_dist_X   = np.sqrt(cruise_speed**2 - 2.54**2) * (304.8/2.54)
+descent_dist_X = np.sqrt(cruise_speed**2 - 1.524**2) * (304.8/1.524)
+cruise_range   = mission_range - climb_dist_X - descent_dist_X
+
 # Take-off from 5000 ft ASL or 1500 m
 mission = Mission(takeoff_altitude=0.0)
 # mission.add_segment('Hover Climb', kind='HoverStay', duration=120.0, n_discrete=5)
-mission.add_segment(name='Hover Climb', kind='HoverClimbConstantSpeed', speed=2.54, distance=304.8, n_discrete=10)
-mission.add_segment('Cruise', kind='CruiseConstantSpeed', AoA=5, speed=cruise_speed, distance=mission_range, n_discrete=5)
-mission.add_segment(name='Hover Descent', kind='HoverDescentConstantSpeed', speed=1.524, distance=304.8, n_discrete=10)
+mission.add_segment(name='Hover Climb', kind='HoverClimbConstantSpeed', speed=2.54, distance=152.4, n_discrete=10)
+mission.add_segment(name='Constant Climb', kind='ClimbConstantVyConstantVx', distance_Y=304.8, speed_Y=2.54, speed=cruise_speed, n_discrete=10)
+mission.add_segment('Cruise', kind='CruiseConstantSpeed', AoA=5, speed=cruise_speed, distance=cruise_range, n_discrete=10)
+mission.add_segment(name='Constant Descent', kind='DescentConstantVyConstantVx', distance_Y=304.8, speed_Y=1.524, speed=cruise_speed, n_discrete=10)
+mission.add_segment(name='Hover Descent', kind='HoverDescentConstantSpeed', speed=1.524, distance=152.4, n_discrete=10)
 # mission.add_segment('Hover Descent', kind='HoverStay', duration=120.0, n_discrete=5)
 # plot_mission_parameters(mission, print_info=False)
 
@@ -35,6 +43,8 @@ results = analysis.evaluate()
 print('Power segment_1 = ', results.get_val('Power|segment_1', 'W'))
 print('Power segment_2 = ', results.get_val('Power|segment_2', 'W'))
 print('Power segment_3 = ', results.get_val('Power|segment_3', 'W'))
+print('Power segment_4 = ', results.get_val('Power|segment_4', 'W'))
+print('Power segment_5 = ', results.get_val('Power|segment_5', 'W'))
 print('Required energy = ', results.get_val('Energy|entire_mission', 'W*h'))
 # print(results.get_val('Power|Propeller|maximum'))
 # print(results.get_val('Power|LiftRotor|maximum'))
