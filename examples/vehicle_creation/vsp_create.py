baaeec63c51097a27a4126bd4c2acd40dacd7216
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-08-17 14:47:46 +0800

    Added examples for multirotor and liftpluscruise vehicle creations

--- examples/vehicle_creation/vsp_create.py
+++ examples/vehicle_creation/vsp_create.py
@@ -1,60 +1,52 @@
 import openvsp as vsp
-from MCEVS.Applications.OpenVSP.Components.Fuselage import NASA_LPC_Fuselage
-from MCEVS.Applications.OpenVSP.Components.Wing import NASA_LPC_Wing
-from MCEVS.Applications.OpenVSP.Components.Tail import NASA_LPC_Horizontal_Tail, NASA_LPC_Vertical_Tail
-from MCEVS.Applications.OpenVSP.Components.Landing_Gear import NASA_LPC_Landing_Gear
-from MCEVS.Applications.OpenVSP.Components.Rotor import NASA_LPC_Lift_Rotor, NASA_LPC_Propeller
 from MCEVS.Applications.OpenVSP.Components.Payload import Human
-from MCEVS.Applications.OpenVSP.Components.Boom import NASA_LPC_Boom
+from MCEVS.Applications.OpenVSP.Components.Fuselage import NASA_QR_Fuselage
+from MCEVS.Applications.OpenVSP.Components.Boom import NASA_QR_Boom
+from MCEVS.Applications.OpenVSP.Components.Rotor import NASA_QR_Lift_Rotor
+from MCEVS.Applications.OpenVSP.Components.Landing_Gear import NASA_QR_Landing_Gear
 
-n_pax = 4
+# n_pax = 4
 
-Human(N_PAX=n_pax)
+# Human(N_PAX=n_pax, config='Multirotor')
 
-if n_pax == 4:
-	l_fuse 		= 9.0	 # m
-	d_fuse_max 	= 1.8 	 # m
+# if n_pax == 1:
+# 	l_fuse 		= 2.074	 # m
+# 	d_fuse_max 	= 1.4 	 # m
+# elif n_pax == 4:
+# 	l_fuse 		= 5.2	 # m
+# 	d_fuse_max 	= 1.8 	 # m
 
-NASA_LPC_Fuselage(l_fuse, d_fuse_max)
-# Cessna 172: AR 7.32, S16.2
-NASA_LPC_Wing(area=16.2, aspect_ratio=7.32, l_fuse=l_fuse)
-NASA_LPC_Horizontal_Tail(area=2.0, aspect_ratio=2.0, l_fuse=l_fuse)
-NASA_LPC_Vertical_Tail(area=2.5, aspect_ratio=1.2, l_fuse=l_fuse)
-NASA_LPC_Landing_Gear(l_strut=0.3, l_fuse=l_fuse, d_fuse_max=d_fuse_max)
-NASA_LPC_Lift_Rotor(n_lift_rotor=8, n_blade=2, r_lift_rotor=1.0, l_fuse=l_fuse, wing_S=16.2, wing_AR=7.32)
-NASA_LPC_Propeller(n_propeller=1, n_blade=4, r_propeller=1.0, l_fuse=l_fuse)
-NASA_LPC_Boom(n_lift_rotor=8, l_fuse=l_fuse, wing_S=16.2, wing_AR=7.32)
-vsp.WriteVSPFile('vehicle.vsp3')
+# # # NASA_QR_Fuselage()
+# NASA_QR_Fuselage(l_fuse=l_fuse, d_fuse_max=d_fuse_max)
+# NASA_QR_Boom(n_lift_rotor=4, l_fuse=l_fuse, d_fuse_max=d_fuse_max)
+# NASA_QR_Lift_Rotor(n_lift_rotor=4, r_lift_rotor=2.0, n_blade=5, l_fuse=l_fuse, d_fuse_max=d_fuse_max)
+# NASA_QR_Landing_Gear(l_strut=0.3, l_fuse=l_fuse, d_fuse_max=d_fuse_max)
+# vsp.WriteVSPFile('vehicle.vsp3')
 
-# vsp.ReadVSPFile('NASA_LPC/prop_example.vsp3')
+vsp.ReadVSPFile('multirotor.vsp3')
 
-# wing_id = vsp.FindGeoms()[0]
-# # # wing_surf = vsp.GetXSecSurf(wing_id, 0)
-# # # xsec_num = vsp.GetNumXSec(wing_surf)
-# # # # # # print(xsec_num)
-# # # for i in range(1,xsec_num-1):
-# # # 	xsec = vsp.GetXSec(wing_surf, i)
-# # # 	print(vsp.GetParmVal(vsp.GetXSecParm(xsec, 'Circle_Diameter')))
-# # # 	vsp.GetXSecParm(xsec, 'Height')
-# # # for i in range(1,4):
-# # 	# print(vsp.GetParmVal(wing_id, 'Circle_Diameter', f'XSec_{i}'))
-# # # # 	# print(vsp.GetParmVal(wing_id, 'SectTess_U', f'XSec_{i}'))
-# # # # 	# print(vsp.GetParmVal(wing_id, 'InLEMode', f'XSec_{i}'))
-# # # # 	# print(vsp.GetParmVal(wing_id, 'InLEStrength', f'XSec_{i}'))
-# # # # 	# print(vsp.GetParmVal(wing_id, 'InLESweep', f'XSec_{i}'))
-# # # # 	# print(vsp.GetParmVal(wing_id, 'InLEDihedral', f'XSec_{i}'))
+comp_id = vsp.FindGeoms()[2]
+# # print(comp_id)
+comp_surf = vsp.GetXSecSurf(comp_id, 0)
+# xsec_num = vsp.GetNumXSec(comp_surf)
+# # print(xsec_num)
+# for i in range(1,xsec_num-1):
+# 	xsec = vsp.GetXSec(comp_surf, i)
+# # 	# print(vsp.GetParmVal(comp_id, 'Dihedral', f'XSec_{i}'))
+# 	print(vsp.GetParmVal(vsp.GetXSecParm(xsec, 'Ellipse_Height')))
 
+print(vsp.GetParmVal(comp_id, 'Rots_Attach_Flag', 'Attach'))
 
 
 # # # # print(vsp.GetParmVal(wing_id,'Diameter','XSecCurve1'))
-# # # xsec = vsp.GetXSec(wing_surf, 1)
-# parm_groups 	= vsp.FindContainerGroupNames(wing_id)
-# print(parm_groups)
+# xsec = vsp.GetXSec(comp_surf, 1)
+parm_groups 	= vsp.FindContainerGroupNames(comp_id)
+print(parm_groups)
 
-# parm_ids 	= vsp.FindContainerParmIDs(wing_id) 
-# for parm_id in parm_ids:
-# 	parm_name = vsp.GetParmName(parm_id)
-# 	print(parm_name)
+parm_ids 	= vsp.FindContainerParmIDs(comp_id) 
+for parm_id in parm_ids:
+	parm_name = vsp.GetParmName(parm_id)
+	print(parm_name)
 
 # print(vsp.GetParmVal(wing_id, 'PropMode', 'Design'))
 # print(vsp.GetParmVal(wing_id, 'Diameter', 'Design'))
@@ -62,17 +54,5 @@ vsp.WriteVSPFile('vehicle.vsp3')
 
 # # print(vsp.GetParmVal(wing_id, 'Angle', 'Design'))
 # # # print(vsp.GetParmVal(wing_id, 'Chevron_Type', parm_groups[3]))
-
-
-
-
-# # # # # xsec = vsp.GetXSec(wing_surf, 0)
-# # # parm_ids = vsp.FindContainerParmIDs( wing_id )
-
-# # # print(parm_ids)
-# # # for i in range(len(parm_ids)):
-# # #     name_id = vsp.GetParmName( parm_ids[i] ) + ": " + parm_ids[i] + "\n"
-# # #     print( name_id )
-
 
 
