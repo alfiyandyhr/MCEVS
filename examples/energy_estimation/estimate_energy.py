ec916c142f8310080c89667112fb390567ffd686
Alfiyandy Hariansyah <alfiyandyhariansyah@gmail.com>
2024-12-18 17:58:23 +0800

    updated energy estimation examples

--- examples/energy_estimation/estimate_energy.py
+++ examples/energy_estimation/estimate_energy.py
@@ -7,8 +7,8 @@ from MCEVS.Analyses.Energy.Analysis import EnergyAnalysis
 from MCEVS.Utils.Plots import plot_mission_parameters, plot_performance_by_segments
 
 # Mission requirement
-mission_range = 30000 # m
-cruise_speed = 30.0 # m/s
+mission_range = 96000 # m
+cruise_speed = 50.0 # m/s
 payload_weight = 400.0
 
 # Cruise range
@@ -25,33 +25,32 @@ mission.add_segment('Cruise', kind='CruiseConstantSpeed', AoA=5, speed=cruise_sp
 mission.add_segment(name='Constant Descent', kind='DescentConstantVyConstantVx', distance_Y=304.8, speed_Y=1.524, speed=cruise_speed, n_discrete=10)
 mission.add_segment(name='Hover Descent', kind='HoverDescentConstantSpeed', speed=1.524, distance=152.4, n_discrete=10)
 # mission.add_segment('Hover Descent', kind='HoverStay', duration=120.0, n_discrete=5)
-plot_mission_parameters(mission, print_info=False, save_fig=True)
+plot_mission_parameters(mission, print_info=False, save_fig=False)
 
-# # Constants
-# constants = EarthGravityAndAtmosphere('US_Standard_1976').compute_constants(altitude=mission.takeoff_altitude)
+# Constants
+constants = EarthGravityAndAtmosphere('US_Standard_1976').compute_constants(altitude=mission.takeoff_altitude)
 
-# # Design variables (aircraft design and operation)
-# design_var1 = {'r_lift_rotor': 1.0, 'cruise_speed': cruise_speed, 'rotor_advance_ratio': 0.3}
-# design_var2 = {'wing_area': 8.0, 'wing_aspect_ratio': 10.0,
-# 			  'r_lift_rotor': 1.0, 'r_propeller': 1.0,
-# 			  'cruise_speed': cruise_speed, 'propeller_advance_ratio': 1.0}
+# Design variables (aircraft design and operation)
+design_var1 = {'r_lift_rotor': 1.5, 'cruise_speed': cruise_speed, 'rotor_advance_ratio': 0.3}
+design_var2 = {'wing_area': 16.2, 'wing_aspect_ratio': 7.32,
+			  'r_lift_rotor': 1.0, 'r_propeller': 1.0,
+			  'cruise_speed': cruise_speed, 'propeller_advance_ratio': 1.0}
 
-# vehicle1 = StandardMultirotorEVTOL(design_var1, mtow=863.78292543)
-# vehicle2 = StandardLiftPlusCruiseEVTOL(design_var2, mtow=983.1065278)
+vehicle1 = StandardMultirotorEVTOL(design_var1, mtow=1021.6295)
+vehicle2 = StandardLiftPlusCruiseEVTOL(design_var2, mtow=1143.25918)
 
-# # Analysis
-# analysis = EnergyAnalysis(vehicle=vehicle2, mission=mission, constants=constants)
-# analysis.evaluate(record=True)
+# Analysis
+analysis = EnergyAnalysis(vehicle=vehicle2, mission=mission, constants=constants, fidelity={'aero':1})
+results = analysis.evaluate(record=True)
 
-# plot_performance_by_segments(mission=mission, vehicle=vehicle2)
+plot_performance_by_segments(mission=mission, vehicle=vehicle2)
 
-
-# # print(results.get_val('Power|segment_1', 'kW'))
-# # print(results.get_val('Power|segment_2', 'kW'))
-# # print(results.get_val('Power|segment_3', 'kW'))
-# # print(results.get_val('Power|segment_4', 'kW'))
-# # print(results.get_val('Power|segment_5', 'kW'))
-# # print(results.get_val('Energy|entire_mission', 'kW*h'))
+print(f"Power|segment_1 = {results.get_val('Power|segment_1', 'kW')[0]} kW")
+print(f"Power|segment_2 = {results.get_val('Power|segment_2', 'kW')[0]} kW")
+print(f"Power|segment_3 = {results.get_val('Power|segment_3', 'kW')[0]} kW")
+print(f"Power|segment_4 = {results.get_val('Power|segment_4', 'kW')[0]} kW")
+print(f"Power|segment_5 = {results.get_val('Power|segment_5', 'kW')[0]} kW")
+print(f"Energy|entire_mission = {results.get_val('Energy|entire_mission', 'kW*h')[0]} kWh")
 
 
 
