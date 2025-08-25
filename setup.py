d0fadd1fb6cc4524506b65641ecbe838d57aac91
alfiyandyhr <alfiyandyhariansyah@gmail.com>
2025-08-25 21:49:14 +0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     license="",
     packages=["MCEVS"],
     package_dir={"": "src"},
-    install_requires=["numpy>=2.2.5", "pandas>=2.2.3", "matplotlib>=3.10.1", "scipy>=1.15.2", "openmdao>=3.38.0", "pymoo>=0.6.1.3", "openvsp>=3.42.3"]
+    install_requires=["numpy", "pandas>=2.2.3", "matplotlib>=3.10.1", "scipy>=1.15.2", "openmdao>=3.38.0", "pymoo>=0.6.1.3", "openvsp>=3.42.3"]
 )
